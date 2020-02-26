0598aa79e6ad0e8bacfc2d050e42309fb75e802a
Georg A. G. Notter <georg.notter@agenterp.com>
2020-02-26 14:27:10 +0100

    add(onchange_bug): added onchange_bug demonstration module

---
+++ onchange_bug/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Onchange Bug",
+    "version": "11.0.1.0.0",
+    "summary": """ The Onchange Bug Module """,
+    "category": "development",
+    "author": "Agent ERP GmbH",
+    "website": "https://www.agenterp.com",
+    "depends": ["base"],
+    "data": ["security/ir.model.access.csv", "views/base_order_view.xml"],
+    "test": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
---
+++ onchange_bug/models/base_order.py
@@ -0,0 +1,39 @@
+from odoo import models, api, fields
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class BaseOrder(models.Model):
+    _name = "base.order"
+    _description = "Base Order"
+
+    name = fields.Char()
+
+    base_order_tour_ids = fields.One2many("base.order.tour", "base_order_id")
+    base_order_cost_ids = fields.One2many("base.order.cost", "base_order_id")
+
+
+class BaseOrderTour(models.Model):
+    _name = "base.order.tour"
+    _description = "Base Order Tour"
+
+    base_order_id = fields.Many2one("base.order")
+    distance = fields.Float()
+
+
+class BaseOrderCost(models.Model):
+    _name = "base.order.cost"
+    _description = "Base Order Cost"
+
+    base_order_id = fields.Many2one("base.order")
+    base_order_tour_id = fields.Many2one("base.order.tour")
+
+    @api.depends("base_order_tour_id", "base_order_tour_id.distance")
+    def _get_price(self):
+        _logger.info("_get_price results for %s", self)
+        for rec in self:
+            rec.price = rec.base_order_tour_id.distance * 10
+            _logger.info("_get_price results in %s", rec.price)
+
+    price = fields.Float(store=True, compute="_get_price")
